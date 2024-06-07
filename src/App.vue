33f0e7264182b135112e13c5d69243a3956c3abe
Jesse Wang <jiojiodibulido@gmail.com>
2024-06-07 15:09:49 +0800

    update on feature1

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div>jesse wang</div>
+    <p>update on feature1</p>
   </div>
 </template>
 
